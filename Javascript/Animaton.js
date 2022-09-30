9bdcc5164dfa9a8edcfbc09aaa0bfdd9817b9e71
Austin <112780937+Alily223@users.noreply.github.com>
2022-09-29 23:53:37 -0400

    Co-authored-by: mcdade007 <mcdade007@users.noreply.github.com> Co-authored-by: blitzz12 <blitzz12@users.noreply.github.com> Co-authored-by: Alex Abreu <abreu218@users.noreply.github.com> Co-authored-by: JohnTheTeke <JohnTheTeke@users.noreply.github.com> Co-authored-by: markestrada8 <markestrada8@users.noreply.github.com>

--- Javascript/Animaton.js
+++ Javascript/Animaton.js
@@ -1,12 +1,18 @@
 const element = document.getElementById('BOL')
 const answer = [ 'yes', 'no', 'maybe', 'try again']
 
+
+
 function Animate(){
     element.classList.add('ball-outer-layer-animation'); // start animation
     setTimeout(() => {element.classList.remove('ball-outer-layer-animation');}, 3000) // Remove Animation Class
     randomAnswer = Math.floor(Math.random() * answer.length);
-    console.log(answer[randomAnswer])
-    return answer[randomAnswer]
+    // console.log(answer[randomAnswer])
+    // return answer[randomAnswer]
+    setTimeout (() => {document.getElementById("eightBall").innerHTML = answer[randomAnswer];}, 3000)
+    
+    
+
 }
 
-
+//  = Animate();
